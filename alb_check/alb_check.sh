ca93b17986ac7ef9612fd1c41efb835b3a0a5f99
daikikuramoto <fp.bkn.0811@gmail.com>
2020-04-17 09:54:12 +0900

    インデント修正

--- alb_check/alb_check.sh
+++ alb_check/alb_check.sh
@@ -20,28 +20,28 @@ alb_show_health() {
       --query 'TargetGroups[].[TargetGroupArn]' \
       --output text` || return $?
 
-      #Target Groupsがなければalbを出力
-      if [ -z "$target_groups" ]; then
+    #Target Groupsがなければalbを出力
+    if [ -z "$target_groups" ]; then
         echo "$alb_arns ターゲットグループ無し"
-      fi
+    fi
 
-      #Target Groupsに紐づいたホストの状態を一覧表示
-      for target_group_arn in ${target_groups[@]}
-      do
-        local target_group_name=`sed 's%^.*:.*/\(.*\)\.*/.*%\1%' <<< $target_group_arn`
+    #Target Groupsに紐づいたホストの状態を一覧表示
+    for target_group_arn in ${target_groups[@]}
+    do
+      local target_group_name=`sed 's%^.*:.*/\(.*\)\.*/.*%\1%' <<< $target_group_arn`
         
-        #可視性のため、EC2のNameタグを取得する
-        local instance=`aws elbv2 describe-target-health \
-          --target-group-arn $target_group_arn \
-          --profile $profile \
-          --query 'TargetHealthDescriptions[].[Target.Id,TargetHealth.State]' \
-          --output text`
+      #可視性のため、EC2のNameタグを取得する
+      local instance=`aws elbv2 describe-target-health \
+        --target-group-arn $target_group_arn \
+        --profile $profile \
+        --query 'TargetHealthDescriptions[].[Target.Id,TargetHealth.State]' \
+        --output text`
 
-          #$instanceに何もなければ$alb_arnsを出力
-          if [ -z "$instance" ]; then
-            echo "$alb_arns インスタンス無し"
-          fi
-      done
+      #$instanceに何もなければ$alb_arnsを出力
+      if [ -z "$instance" ]; then
+        echo "$alb_arns インスタンス無し"
+      fi
+  done
 }
 
 #Profileエラー用
